f12a2fdbd29fe3ec7dac68bbd0063df187903984
Laurent Bachelier <laurent@bachelier.name>
2011-05-19 21:30:05 +0200

    The TestSuite can save an XML result

--- test/TestSuite.php
+++ test/TestSuite.php
@@ -4,7 +4,11 @@ require_once(dirname(__FILE__).'/../lib/external/lime.php');
 $tests = new lime_harness(array('verbose'=>true));
 $tests->register_glob(dirname(__FILE__).'/unit/*Test.php');
 $tests->run();
-if ($tests->stats['failed_tests'])
+if (in_array('--xml', $argv))
+{
+  file_put_contents(dirname(__FILE__).'/../test-report.xml', $tests->to_xml());
+}
+elseif ($tests->stats['failed_tests'])
 {
   exit(1);
 }
